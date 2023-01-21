974f2a61de045cc9b2fc8b3eeaf421663f572be1
Dan Kirkham <herecy@live.co.uk>
2023-01-21 17:51:53 +0000

    Remove cf

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,9 @@
 export default function Index() {
-  return <h1 className="text-xl">Test heading</h1>;
+  return (
+    <section className="flex justify-center mt-10 w-full">
+      <label className="w-96 text-gray-700">
+        Location <input placeholder="Brisbane" className="block w-full rounded-md px-3 py-2 mt-1" />
+      </label>
+    </section>
+  );
 }
--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "cloudflare-pages",
-  server: "./server.js",
-  devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
 };
