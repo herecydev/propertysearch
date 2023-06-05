845461c7c47bbea42b74830766cae7790c1d7b51
Dan Kirkham <herecy@live.co.uk>
2023-06-05 19:43:08 +0100

    Font weights

--- app/components/propertyCard.tsx
+++ app/components/propertyCard.tsx
@@ -10,22 +10,32 @@ const PropertyCard = ({
 }) => (
   <article
     data-testid={`property-${property.id}`}
-    className={`bg-white rounded-md flex flex-col max-w-xl h-full ${
-      summary && "hover:bg-emerald-300"
+    className={`bg-white rounded-md flex flex-col max-w-xl h-full transition-all duration-200 ease-in ${
+      summary && "group hover:bg-emerald-300"
     }`}
   >
-    <img className="rounded-t-md" src={property.image} />
-    <div className={`p-4 ${!summary && "mx-auto"}`}>
-      <header className="flex justify-between gap-4 mb-2 text-lg">
-        <h1 className="font-bold">{property.name}</h1>
-        <span>{currencyFormat.format(property.price)}</span>
+    <picture className="rounded-t-md overflow-hidden">
+      <img
+        className="group-hover:scale-110 transition-all duration-200 ease-in"
+        src={property.image}
+        alt="alt text"
+      />
+    </picture>
+    <div className={`p-6 ${!summary && "mx-auto"}`}>
+      <header className="flex justify-between gap-4 mb-3 text-lg">
+        <h1 className="font-normal uppercase">{property.name}</h1>
+        <span className="font-semibold text-teal-700">
+          {currencyFormat.format(property.price)}
+        </span>
       </header>
       {summary ? (
-        <p>{property.shortDescription}</p>
+        <p className="font-light">{property.shortDescription}</p>
       ) : (
         <div className="space-y-4">
           {property.description.map((paragraph, index) => (
-            <p key={index}>{paragraph}</p>
+            <p className="font-light" key={index}>
+              {paragraph}
+            </p>
           ))}
         </div>
       )}
