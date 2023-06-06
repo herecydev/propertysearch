4b19f765372436bfdeb266b2776988deadb3f36d
Dan Kirkham <herecy@live.co.uk>
2023-06-06 16:58:05 +0100

    Check log

--- app/data/properties.server.ts
+++ app/data/properties.server.ts
@@ -30,11 +30,11 @@ type PropertyDetailQuery = PropertyBaseQuery & {
 
 const fetchContent = async <T>(body: string): Promise<T> => {
   console.log("hello");
-  console.log(process.env.CONTENTFUL_SPACE_ID);
+  console.log(process.env.CONTENTFUL_ACCESS_TOKEN);
   
   
   const response = await fetch(
-    `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`,
+    `https://graphql.contentful.com/content/v1/spaces/q01urmjqhd08`,
     {
       method: "POST",
       headers: {
