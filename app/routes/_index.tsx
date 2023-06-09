9eefed1171f9b52c6df704725111ad4cbecc7e2b
Dan Kirkham <herecy@live.co.uk>
2023-06-09 13:01:57 +0100

    Add basic filtering

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -25,6 +25,8 @@ const Index = () => {
   const { properties, favouriteProperties } = useLoaderData<typeof loader>();
   const [searchParams] = useSearchParams();
   const [search, setSearch] = useState(searchParams.get("search") || "");
+  const [favouritesOnly, setFavouritesOnly] = useState(false);
+  const favouritePropertiesSet = new Set(favouriteProperties);
 
   return (
     <main>
@@ -32,19 +34,29 @@ const Index = () => {
         <Search search={search} setSearch={setSearch} />
       </div>
       {properties.length ? (
-        <section
-          data-testid="properties"
-          className="xl:container mx-auto grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
-        >
-          {properties.map((property) => (
-            <PropertyCard
-              key={property.id}
-              property={property}
-              isFavourited={favouriteProperties.some(
-                (favourite) => favourite === property.id
-              )}
+        <section data-testid="properties" className="xl:container mx-auto">
+          <label className="text-lg">
+            <input
+              type="checkbox"
+              className="mr-1 mb-4 w-4 h-4 accent-emerald-300"
+              checked={favouritesOnly}
+              onChange={() => setFavouritesOnly(!favouritesOnly)}
             />
-          ))}
+            Just my favourites
+          </label>
+          <div className="grid justify-items-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {properties
+              .filter((property) =>
+                favouritesOnly ? favouritePropertiesSet.has(property.id) : true
+              )
+              .map((property) => (
+                <PropertyCard
+                  key={property.id}
+                  property={property}
+                  isFavourited={favouritePropertiesSet.has(property.id)}
+                />
+              ))}
+          </div>
         </section>
       ) : (
         <div className="text-center">
