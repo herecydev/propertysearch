8b0c3cf9beca7a79181ee1c6c084681d500cca1e
Dan Kirkham <herecy@live.co.uk>
2023-06-11 09:16:22 +0100

    Try a focus

--- cypress/e2e/propertysearch.cy.ts
+++ cypress/e2e/propertysearch.cy.ts
@@ -25,7 +25,7 @@ describe("Property search", () => {
       cy.get("[data-testid='search']").within(() => {
         cy.contains("Find the perfect property today to buy or rent");
 
-        cy.get("input").should("not.be.disabled").type(firstProperty.title);
+        cy.get("input").focus().type(firstProperty.title);
         cy.get("button").click();
       });
 
